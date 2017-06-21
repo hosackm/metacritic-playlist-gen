5264218d3b607471b43ed85562f9ecc19bf981fa
Matt Hosack <hosack.matt@gmail.com>
2017-06-21 11:46:47 -0700

    Adding requirements.txt.  Adding metacritic parsing module.  Adding tests for metacritic parsing module.  Adding sample html to use for testing so that html requests don't have to be made during testing

---
+++ mpgen/metacritic.py
@@ -0,0 +1,94 @@
+from datetime import datetime
+from bs4 import BeautifulSoup as Soup
+
+
+def parse_albums_from_metacritic_html(html):
+    soup = Soup(html, "html.parser")
+    soup.find("ol", {"class": "list_products"})
+
+    return [Album.from_list_item(list_item)
+            for list_item
+            in soup.findAll("li", {"class": "product"})]
+
+
+class Album:
+    """
+    Represents an album parsed from Metacritic's site.  Each album
+    has a date, artist, title, and rating associated with it
+
+    If an album is not yet rated (usually for upcoming albums) the rating is
+    assumed to be 0 (harsh)
+    """
+    def __init__(self, date, rating, title, artist):
+        self.date = date
+        self.artist = artist
+        self.rating = rating
+        self.title = title
+
+    @classmethod
+    def from_list_item(cls, li):
+        """
+        Parse date, rating, title, and artist of from an <li> tag soup object
+
+        ex:
+        <li>
+          <div class="product_score">100</div>
+          <div class="product_title">Some Title</div>
+          <li class="release_date"><span class="data">Jan 12</span></li>
+          <li class="product_artist"><span class="data">Some Artist</span></li>
+        </li>
+
+        would parse to:
+            Jan 12 (current or previous four-digit year)
+            Some Title
+            Some Artist
+            100
+        """
+        rating_text = li.find("div", {"class": "product_score"}).text.strip()
+        title_text = li.find("div", {"class": "product_title"}).text.strip()
+        date_text = li.find("li",
+                            {"class": "release_date"}
+                            ).find("span",
+                                   {"class": "data"}).text.strip()
+        artist_text = li.find("li",
+                              {"class": "product_artist"}
+                              ).find("span", {"class": "data"}).text.strip()
+
+        title = MetaTitle(title_text)
+        artist = MetaArtist(artist_text)
+
+        # convert rating in string form to int. 'tbd' is converted to 0
+        try:
+            rt = int(rating_text)
+        except ValueError:
+            rt = 0
+        finally:
+            rating = MetaRating(rt)
+
+        date = MetaDate.strptime(date_text, "%b %d")
+        # assume the the year is the current year
+        date = date.replace(year=datetime.now().year)
+
+        # metacritic doesn't report albums 3 months ahead of their release
+        if date.month > datetime.now().month + 3:
+            # this album must be from last year
+            # ie. it's Jan and this album was release in Dec
+            date = date.replace(year=date.year-1)
+
+        return cls(date=date, rating=rating, title=title, artist=artist)
+
+
+class MetaDate(datetime):
+    pass
+
+
+class MetaRating(int):
+    pass
+
+
+class MetaTitle(str):
+    pass
+
+
+class MetaArtist(str):
+    pass
---
+++ tests/test_metacritic.py
@@ -0,0 +1,103 @@
+import os
+import unittest
+import mpgen.metacritic as metacritic
+from datetime import datetime, timedelta
+from bs4 import BeautifulSoup as Soup
+
+
+class TestMetacriticScrape(unittest.TestCase):
+    def setUp(self):
+        self.albums = metacritic.parse_albums_from_metacritic_html(
+            response_text)
+
+    def test_first_and_last_parsed_correctly(self):
+        """
+        Test the date was parsed from the first and last albums correctly
+        """
+        first_album, last_album = self.albums[0], self.albums[-1]
+
+        self.assertEqual(first_album.date,
+                         datetime.strptime("Jun 23 2017", "%b %d %Y"))
+        self.assertEqual(first_album.artist, "Jeff Tweedy")
+        self.assertEqual(first_album.rating, 77)
+        self.assertEqual(first_album.title, "Together At Last")
+
+        self.assertEqual(last_album.date,
+                         datetime.strptime("Apr 28 2017", "%b %d %Y"))
+        self.assertEqual(last_album.artist, "Los Angeles Police Department")
+        self.assertEqual(last_album.rating, 80)
+        self.assertEqual(last_album.title, "Los Angeles Police Department")
+
+    def test_all_albums_have_valid_data(self):
+        """
+        Make sure data has been parsed correctly
+        """
+        for album in self.albums:
+            assert album.artist != ""
+            assert album.title != ""
+            assert album.date is not None
+            assert 0 <= album.rating <= 100
+
+    def test_correct_number_albums_parsed(self):
+        assert len(self.albums) == 200
+
+    def test_january_december_year_calculation(self):
+        """
+        Metacritic does not display dates so the MetaDate must infer the date
+        if the current month is January and the album was released in December
+        we must make sure the year of the album is one less than the current
+        year
+        """
+        now = datetime.now()
+        more_than_three_months_ahead = now + timedelta(weeks=16)
+        li_html = fake_product_html.format(
+            title="Fake Title",
+            date=more_than_three_months_ahead.strftime("%b %d"),
+            artist="Fake Artist",
+            rating=93
+            )
+        li_soup = Soup(li_html, "html.parser")
+
+        album = metacritic.Album.from_list_item(li_soup)
+
+        self.assertEqual(album.date.year + 1, now.year)
+
+    def test_tbd_rating_parses_to_zero(self):
+        no_rating_html = fake_product_html.format(
+            date="Dec 25",
+            title="",
+            artist="",
+            rating="tbd")
+        nr_soup = Soup(no_rating_html, "html.parser")
+        album = metacritic.Album.from_list_item(nr_soup)
+
+        self.assertEqual(album.rating, 0)
+
+    def test_can_filter_by_date(self):
+        most_recent_date = datetime.strptime("Jun 21 2017", "%b %d %Y")
+        date_one_month_ago = most_recent_date - timedelta(weeks=4)
+
+        one_month_old_albums = list(filter(
+            lambda a: date_one_month_ago <= a.date <= most_recent_date,
+            self.albums))
+
+        self.assertTrue(len(one_month_old_albums) < len(self.albums))
+
+
+# read local copy of metacritic site from June 21, 2017 contains 200 albums
+response_text = ""
+filepath = os.path.join(os.path.dirname(__file__), "metacritic_sample.html")
+with open(filepath, encoding="utf8") as f:
+    response_text = f.read()
+
+fake_product_html = """
+<li>
+  <div class="product_score">{rating}</div>
+  <div class="product_title">{title}</div>
+  <li class="release_date"><span class="data">{date}</span></li>
+  <li class="product_artist"><span class="data">{artist}</span></li>
+</li>
+"""
+
+if __name__ == "__main__":
+    unittest.main()
